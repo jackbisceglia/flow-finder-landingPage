43a5ee610b284dfff2f9e6e32d97364c5d88d11e
Jack Bisceglia <jackbisceglia2000@gmail.com>
2022-10-23 18:32:54 -0400

    feat: add clean up and add animation for homescreen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,53 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Head from "next/head";
 import Image from "next/image";
 import type { NextPage } from "next";
 
-const Home: NextPage = () => {
-  const [apiRes, setApiRes] = useState<Record<string, string>>({});
-  const [dataLoading, setDataLoading] = useState(true);
+const useFadeOnPageLoad = () => {
+  const [fadeOpacity, setFadeOpacity] = useState(0);
 
   useEffect(() => {
-    (async () => {
-      const res = await fetch("/api/main");
-      const resJson = await res.json();
-
-      setApiRes(resJson);
-      setDataLoading(false);
-    })();
+    setFadeOpacity((_) => 100);
   }, []);
+
+  return { fadeOpacity };
+};
+
+const getCoefficient = () => (Math.random() < 0.5 ? -1 : 1);
+
+const getAngle = (angle: number) => getCoefficient() * angle;
+
+const LandingScreenSection = () => {
+  const { fadeOpacity } = useFadeOnPageLoad();
+  return (
+    <section className="test-bg background-animate flex h-screen w-full flex-col items-center justify-center overflow-y-hidden bg-neutral-900">
+      <p className="absolute top-10 left-10 text-blue-200">
+        A Product By Pathos
+      </p>
+      <h1
+        className={`py-0 text-6xl font-bold text-blue-600 opacity-${fadeOpacity} transition-opacity duration-1000`}
+      >
+        FlowFinder
+      </h1>
+      <p
+        className={`font-base py-0 text-xl text-blue-200 opacity-${fadeOpacity} transition-opacity duration-1000`}
+      >
+        Find Your Flow
+      </p>
+      <div className="py-4"></div>
+      <p
+        className={`text-lg text-blue-200 opacity-${fadeOpacity} cursor-default transition-all duration-2500 hover:rotate-[2.5deg]`}
+      >
+        bridging the gap between <span className="text-blue-500">people</span>{" "}
+        and their <span className="py-2 pt-0 text-blue-500 ">basic needs</span>
+      </p>
+    </section>
+  );
+};
+
+const Home: NextPage = () => {
+  // py-4 px-8 md:px-32
 
   return (
     <>
@@ -27,13 +58,14 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="flex min-h-screen w-full flex-col items-center ">
-        <nav className="flex w-full flex-col items-center justify-center bg-zinc-900 py-3 px-12 text-blue-500  sm:px-36">
+        {/* <nav className="flex w-full flex-col items-center justify-center bg-zinc-900 py-3 px-12 text-blue-500  sm:px-36">
           <h1 className="py-0 text-5xl font-bold ">FlowFinder</h1>
           <p className="font-base text-regular py-0 text-blue-200">
             Find Your Flow
           </p>
-        </nav>
-        <div className="flex h-full w-full flex-col items-center  py-4 px-8 md:px-32">
+        </nav> */}
+        <div className="flex h-full w-full flex-col items-center ">
+          <LandingScreenSection />
           <section className="flex w-full flex-col gap-6  py-2 leading-none sm:w-4/5 lg:flex-row">
             <div className="w-full lg:w-3/5">
               <h3 className="py-2 text-4xl font-black text-neutral-900">
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      transitionDuration: {
+        1500: "1500ms",
+        2000: "2000ms",
+        2500: "2500ms",
+        3000: "3000ms",
+      },
+    },
   },
   plugins: [],
+  safelist: [
+    "opacity-0",
+    "opacity-100",
+    "duration-1000",
+    // {
+    //   pattern: /rotate-[0-9]+/,
+    //   variants: ["hover"],
+    // },
+  ],
 };

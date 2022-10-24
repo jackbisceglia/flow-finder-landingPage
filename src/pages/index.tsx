32750b07bd0d5a5f8d561d0defd88c21f28de657
Jack Bisceglia <jackbisceglia2000@gmail.com>
2022-10-23 21:53:58 -0400

    add more

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
+import { Pitch, Solutions } from "../utils/content";
 import React, { useEffect, useState } from "react";
 
+import { CaretDownIcon } from "@radix-ui/react-icons";
 import Head from "next/head";
 import Image from "next/image";
 import type { NextPage } from "next";
@@ -21,7 +23,7 @@ const getAngle = (angle: number) => getCoefficient() * angle;
 const LandingScreenSection = () => {
   const { fadeOpacity } = useFadeOnPageLoad();
   return (
-    <section className="test-bg background-animate flex h-screen w-full flex-col items-center justify-center overflow-y-hidden bg-neutral-900">
+    <section className="test-bg background-animate flex h-screen w-full snap-y flex-col items-center justify-center overflow-y-hidden bg-neutral-900">
       <p className="absolute top-4 left-4 text-sm text-blue-200  sm:left-10 sm:top-10 sm:text-base">
         A Product By Pathos
       </p>
@@ -42,6 +44,93 @@ const LandingScreenSection = () => {
         bridging the gap between <span className="text-blue-500">people</span>{" "}
         and their <span className="py-2 pt-0 text-blue-500 ">basic needs</span>
       </p>
+      <a href="#section2">
+        <CaretDownIcon className="absolute bottom-10 h-12 w-12 cursor-pointer text-blue-200 transition-colors duration-200 ease-in-out hover:text-blue-500" />
+      </a>
+    </section>
+  );
+};
+
+const TheProductSection = () => {
+  return (
+    <section className="flex w-full  items-center justify-center p-16">
+      <div
+        id="section2"
+        className="flex w-full snap-y flex-col gap-6 sm:w-4/5 lg:flex-row "
+      >
+        <div className=" w-full lg:w-2/3">
+          <h3 className="py-2 pb-2 text-4xl font-black text-neutral-900">
+            The Mission
+          </h3>
+          <div className="pb-6 ">
+            {/* HEADING 1 - Pitch */}
+            <h4 className="text-2xl font-semibold text-blue-700">
+              {Pitch.title}
+            </h4>
+            <p className="font-base py-1 text-xl">
+              {Pitch.missionStatement} {Pitch.context}
+            </p>
+          </div>
+          {/* HEADING 2 - Solutions */}
+          <div className="pb-6 ">
+            <h4 className="text-2xl font-semibold text-blue-700">
+              How do we solve this?
+            </h4>
+            <ol className="list-decimal px-8 ">
+              <li className="font-base py-1 text-xl">
+                <b>{Solutions.a.title}</b> {Solutions.a.content}
+              </li>
+              <li className="font-base py-1 text-xl">
+                <b>{Solutions.b.title}</b> {Solutions.b.content}
+              </li>
+            </ol>
+          </div>
+          <div className="pb-6 ">
+            <h4 className="text-2xl font-semibold text-blue-700">
+              So, why FlowFinder?
+            </h4>
+            <div className=" grid grid-cols-1 gap-6 py-2 2xl:grid-cols-2">
+              <div>
+                <p className=" text-xl font-bold">The Difference</p>
+                <p className="text-lg">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Voluptatem quisquam suscipit eveniet debitis deserunt
+                  temporibus dolorum tempore, voluptatum pariatur soluta quas,
+                  nam odit nesciunt, delectus dignissimos quis laborum excepturi
+                  ipsum! Ratione sint natus aperiam at minima aut expedita,
+                  asperiores vitae a rerum nisi suscipit ad distinctio, impedit
+                  quia quod! Ut cum harum distinctio necessitatibus, quis labore
+                  aliquam impedit blanditiis aliquid.
+                </p>
+              </div>
+              <div>
+                <p className=" text-xl font-bold">The Advantage</p>
+                <p className="text-lg">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+                  dolore doloremque, ab hic veniam voluptas placeat pariatur
+                  fugiat impedit facere doloribus tenetur ex quisquam ad omnis?
+                  Natus recusandae dignissimos veritatis! Iusto incidunt quam
+                  consequuntur? Magni quas, harum veritatis dolorum error
+                  eveniet labore officia tempora quod laudantium nulla adipisci,
+                  animi dignissimos dolore! Vero maxime, officia dolor nam
+                  recusandae nulla quam impedit!
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-full lg:w-1/3">
+          <h3 className="py-2 text-4xl font-black text-neutral-900">The App</h3>
+          <div className="relative h-72 w-72">
+            <Image
+              className=" drag- select-none rounded-full border-2 "
+              src="https://assets.stickpng.com/images/580b57fbd9996e24bc43bf86.png"
+              alt="mockup image"
+              layout="fill"
+            />
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
@@ -57,50 +146,16 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex min-h-screen w-full flex-col items-center ">
+      <main className="flex min-h-screen w-full flex-col items-center scroll-smooth ">
         {/* <nav className="flex w-full flex-col items-center justify-center bg-zinc-900 py-3 px-12 text-blue-500  sm:px-36">
           <h1 className="py-0 text-5xl font-bold ">FlowFinder</h1>
           <p className="font-base text-regular py-0 text-blue-200">
             Find Your Flow
           </p>
         </nav> */}
-        <div className="flex h-full w-full flex-col items-center ">
+        <div className="flex h-full w-full flex-col items-center scroll-smooth ">
           <LandingScreenSection />
-          <section className="flex w-full flex-col gap-6  py-2 leading-none sm:w-4/5 lg:flex-row">
-            <div className="w-full lg:w-3/5">
-              <h3 className="py-2 text-4xl font-black text-neutral-900">
-                The Mission
-              </h3>
-              <h4 className="text-2xl font-semibold text-blue-700">
-                Take Your Next Trip to the Bathroom—The Easy Way
-              </h4>
-              <p className="font-base py-1 text-xl">
-                (Mission Statement) Lorem ipsum dolor sit, amet consectetur
-                adipisicing elit. Quae ex, iste, odit eius autem consectetur
-                deleniti dicta quas expedita animi similique pariatur atque at,
-                quia quibusdam maiores distinctio voluptates iure!
-              </p>
-              <p className="font-base py-1 text-xl">
-                (Unique Advantage) Lorem ipsum dolor sit, amet consectetur
-                adipisicing elit. Quae ex, iste, odit eius autem consectetur
-                deleniti dicta quas expedita animi similique pariatur atque at,
-                quia quibusdam maiores distinctio voluptates iure!
-              </p>
-            </div>
-            <div className="w-full lg:w-2/5">
-              <h3 className="py-2 text-4xl font-black text-neutral-900">
-                The App
-              </h3>
-              <div className="relative h-72 w-72">
-                <Image
-                  className=" drag- select-none rounded-full border-2 "
-                  src="https://assets.stickpng.com/images/580b57fbd9996e24bc43bf86.png"
-                  alt="mockup image"
-                  layout="fill"
-                />
-              </div>
-            </div>
-          </section>
+          <TheProductSection />
         </div>
       </main>
     </>
@@ -108,3 +163,10 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+// <p className="font-base py-1 text-xl">
+//   FlowFinder is a mobile app that allows users to find the nearest and
+//   most accessible public restrooms. We pull on crowd-sourced data to
+//   aggregate restroom locations, and give our users access to the best
+//   restrooms in their area.
+// </p>
---
+++ src/utils/content.ts
@@ -0,0 +1,22 @@
+const Solutions = {
+  a: {
+    title: "Simple, user-focused restroom discovery.",
+    content:
+      "FlowFinder leverages crowd-sourced restroom data to guide users to nearby restrooms as quickly and as seamlessly as possible.",
+  },
+  b: {
+    title: "Comprehensive restroom reviews.",
+    content:
+      " Powered by the users, FlowFinder thrives on the community's feedback to provide accurate and user-friendly restroom reviews.",
+  },
+};
+
+const Pitch = {
+  title: "Take Your Next Trip to the Bathroom—The Easy Way",
+  missionStatement:
+    "FlowFinder bridges the gap between people and their basic needs.",
+  context:
+    "Searching for a restroom in a public, and sometimes unfamiliar, place can be tough and time consuming. The best restrooms are not always publicly visible. Some business owners will require you to make a purchase for restroom access. And sometimes, the restroom is just simply unclean. FlowFinder aims to fix this.",
+};
+
+export { Solutions, Pitch };

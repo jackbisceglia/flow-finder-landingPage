ed2b602a072b3c144ba7b1985966afc0592bae83
Jack Bisceglia <jackbisceglia2000@gmail.com>
2022-10-26 12:28:58 -0400

    fix'

---
+++ src/pages/api/signup.tsx
@@ -0,0 +1,34 @@
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+
+import { PrismaClient } from "@prisma/client";
+
+const p = new PrismaClient();
+
+const handler: NextApiHandler = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  if (req.method === "POST") {
+    const body = JSON.parse(req.body);
+    if (!body || !body.email) {
+      res.status(400).json({ message: "Missing email" });
+      return;
+    }
+
+    const resource = await p.signUp.create({
+      data: {
+        email: body.email,
+      },
+    });
+
+    if (!resource) {
+      res.send({ error: "Already Signed Up" });
+      return;
+    } else {
+      res.send({ resource });
+      return;
+    }
+  }
+};
+
+export default handler;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -74,7 +74,7 @@ const MockupCarousel = () => {
           <ArrowRightIcon className="h-10 w-10 px-2" />
         </button>
       </div>
-      <div className="relative mt-6 h-[24rem]">
+      <div className="relative mt-6 h-[28.5rem]">
         <Image
           className={`border72 select-none transition-all  duration-1000 ease-in`}
           src={`/MOCKUPS/MOCKUP ${imgNumber}.png`}
@@ -163,46 +163,44 @@ const TheProductSection = () => {
 
 const PricingSection = () => {
   return (
-    <section className="flex w-full  items-center justify-center px-8 pt-12 pb-10 md:px-28 lg:px-44">
-      <div className=" w-full text-center lg:w-2/3">
-        <h3 className="text-4xl font-black text-neutral-900">Pricing</h3>
-        <h4 className="py-4 text-2xl font-semibold text-blue-700">
-          {"Choose what's right for you"}
-        </h4>
-        <div className="flex w-full gap-10 py-3">
-          <div className="w-1/2 rounded-md bg-neutral-900 px-4 py-5">
-            <h1 className="pb-1 text-xl font-extrabold text-blue-100">
-              The Free Plan
-            </h1>
-            <h3 className="text-md  py-1 font-normal text-blue-400">
-              0.00$ per month
-            </h3>
-            <p className="text-lg text-blue-200">
-              FlowFinder sees the free plan as the standard plan. We make sure
-              that the users get all the features of FlowFinder without any
-              limitations. At the free tier, you get all the features you need
-              to experience the full experience of FlowFinder. You can upgrade
-              any time, but no need to rush!
-            </p>
-          </div>
-          <div className="w-1/2 rounded-md bg-neutral-900 px-4 py-5">
-            <h1 className="pb-1 text-xl font-extrabold text-blue-100">
-              The Premium Plan
-            </h1>
-            <h3 className="text-md  py-1 font-normal text-blue-400">
-              1.99$ per month
-            </h3>
-            <p className="text-lg text-blue-200">
-              The premium plan is for the hardcore users. We wholeheartedly
-              believe that the free tier is the best tier, but at our premium
-              tier, we offer:
-              <ul className="py-3 font-semibold text-blue-400">
-                <li>early access to new features</li>
-                <li>an ad-free experience</li>
-                <li>points multipliers</li>
-              </ul>
-            </p>
-          </div>
+    <section className="flex w-full flex-col items-center justify-center px-8 pt-12 pb-10 md:px-28 lg:px-44">
+      <h3 className="text-4xl font-black text-neutral-900">Pricing</h3>
+      <h4 className="py-4 text-2xl font-semibold text-blue-700">
+        {"Choose what's right for you"}
+      </h4>
+      <div className="flex w-full flex-col items-start justify-center gap-8 py-2 text-center md:flex-row lg:w-5/6">
+        <div className=" h-max w-full rounded-md bg-neutral-900 px-8 py-5 md:w-2/5">
+          <h1 className="pb-1 text-xl font-extrabold text-blue-100">
+            The Free Plan
+          </h1>
+          <h3 className="text-md  py-1 font-normal text-blue-400">
+            0.00$ per month
+          </h3>
+          <p className="text-lg text-blue-200">
+            FlowFinder sees the free plan as the standard plan. We make sure
+            that the users get all the features of FlowFinder without any
+            limitations. At the free tier, you get all the features you need to
+            experience the full experience of FlowFinder. You can upgrade any
+            time, but no need to rush!
+          </p>
+        </div>
+        <div className="w-full rounded-md bg-neutral-900 px-4 py-5 md:w-2/5">
+          <h1 className="pb-1 text-xl font-extrabold text-blue-100">
+            The Premium Plan
+          </h1>
+          <h3 className="text-md  py-1 font-normal text-blue-400">
+            1.99$ per month
+          </h3>
+          <p className="text-lg text-blue-200">
+            The premium plan is for the hardcore users. We wholeheartedly
+            believe that the free tier is the best tier, but at our premium
+            tier, we offer:
+          </p>
+          <ul className="py-3 font-semibold text-blue-400">
+            <li>early access to new features</li>
+            <li>an ad-free experience</li>
+            <li>points multipliers</li>
+          </ul>
         </div>
       </div>
     </section>
@@ -210,6 +208,26 @@ const PricingSection = () => {
 };
 
 const JoinTheNewsLetter = () => {
+  const [statusMessage, setStatusMessage] = useState("");
+  const statusHidden = statusMessage === "";
+  const [email, setEmail] = useState("");
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const res = await fetch("/api/signup", {
+      method: "POST",
+      body: JSON.stringify({
+        email: email,
+      }),
+    });
+    const data = await res.json();
+    if (data.error) {
+      setStatusMessage("Couldn't sign up. Try again.");
+    } else {
+      setStatusMessage("Thanks for signing up!");
+    }
+    setEmail("");
+  };
+
   return (
     <section className="flex w-full  items-center justify-center px-8 pt-12 pb-10 md:px-28 lg:px-44">
       <div className=" w-full text-center lg:w-2/3">
@@ -219,17 +237,33 @@ const JoinTheNewsLetter = () => {
         <h4 className="py-4 text-2xl font-medium text-blue-700">
           {"Sign up for early access to FlowFinder"}
         </h4>
-        <form className="w-full py-4">
+        <form onSubmit={handleSubmit} className="w-full py-4">
           <input
+            value={email}
+            onChange={(e) => setEmail(() => e.target.value)}
             className="mb-4 w-full rounded-md bg-blue-100 p-2 text-lg placeholder-stone-500"
             type="email"
             placeholder="email address"
           />
-          <button className="grow rounded-md bg-blue-700 py-2 px-6 text-lg text-stone-50 hover:bg-blue-800 active:bg-red-600">
+          <button className="grow rounded-md bg-blue-700 py-2 px-6 text-lg text-stone-50 hover:bg-blue-800 active:bg-blue-600">
             Sign Up
           </button>
         </form>
+        <p className={`${statusHidden ? "hidden" : ""} text-blue-500`}>
+          {statusMessage}
+        </p>
       </div>
+    </section>
+  );
+};
+
+const GoogleFormSection = () => {
+  return (
+    <section className="flex w-full items-center justify-center px-8 pt-12 pb-10 md:w-5/6 md:px-28 lg:px-44">
+      <iframe
+        className="h-[40rem] w-full"
+        src="https://docs.google.com/forms/d/e/1FAIpQLSewDy-d4YrTqn4tRFlKZot8Iia0aLeO91cwyzLEDDiVmkpBSQ/viewform"
+      ></iframe>
     </section>
   );
 };
@@ -262,7 +296,10 @@ const Home: NextPage = () => {
         <div className="flex h-full w-full flex-col items-center scroll-smooth bg-neutral-900">
           <JoinTheNewsLetter />
         </div>
-        <footer className="test-bg flex w-full flex-col items-center scroll-smooth bg-neutral-900 py-4">
+        <div className="flex h-full w-full flex-col items-center scroll-smooth bg-neutral-200">
+          <GoogleFormSection />
+        </div>
+        <footer className="background-animate test-bg flex w-full flex-col items-center scroll-smooth bg-neutral-800 py-4">
           <h1
             className={`font-regular py-1 text-xl text-blue-600  transition-opacity duration-1000`}
           >
